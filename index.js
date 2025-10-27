fa843f2561dd8a9617d7125d66512a47d14a31e9
akshaykumarsharma0909 <akshaykumarsharma0909@gmail.com>
2025-10-27 22:58:00 +0530

    Add default route and dynamic port for Render

--- index.js
+++ index.js
@@ -123,9 +123,7 @@ app.all("*",(err,req,res,next)=>{
     return next(err);
 });
 */
-app.get("/", (req, res) => {
-  res.send("🚀 Alive Project backend is running successfully on Render!");
-});
+
 
 
 app.use((err, req, res, next)=>{
